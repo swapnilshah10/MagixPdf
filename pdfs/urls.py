6381e504a14b96f578057ee05ac8c1ce15ace597
Swapnil <swapnilshah102002@gmail.com>
2022-11-27 21:15:15 +0530

    created extract text api

--- pdfs/urls.py
+++ pdfs/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('merge/' , merge_file , name='merge'),
     path('reduce/' , reduce_file , name='reduce'),
-    path('upload/' , upload_file , name='upload')
+    path('upload/' , upload_file , name='upload'),
+    path('read/' , read_file , name='read')
 ]
--- pdfs/views.py
+++ pdfs/views.py
@@ -8,13 +8,14 @@ from rest_framework.status import (
 )
 from rest_framework.response import Response
 from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 import PyPDF2
 import time
 from PyPDF2 import PdfReader, PdfWriter
 import requests
 import os 
+from django.http import FileResponse
 
 
 def uploadd(p):
@@ -50,6 +51,21 @@ def pdfmerger(pdfs):
     return uploadd(p)
 
 
+
+def text_extractor(pdfs):
+    text = ""
+    for pdf in pdfs:
+        pdfReader = PyPDF2.PdfFileReader(pdf)
+        for i in range(pdfReader.numPages):
+            pageObj = pdfReader.getPage(i)
+            text += pageObj.extractText()
+    f = open("media/temp/text.txt", "w+")
+    f.write(text)
+    f.close()
+    return "media/temp/text.txt"
+
+
+
 @api_view(['POST'])
 def merge_file(request):
     if request.method == 'POST':
@@ -71,13 +87,33 @@ def reduce_file(request):
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+
 @api_view(['POST'])
 def upload_file(request):
     if request.method == 'POST':
         file = []
         file = (request.FILES['file'])
         pdf = Pdfs.objects.create(Pdfs=file)
-        print(pdf.Pdfs)
+        # print(pdf.Pdfs)
         return Response("http://127.0.0.1:8000/media/"+str(pdf.Pdfs) , status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['POST'])
+def read_file(request):
+    if request.method == 'POST':
+        file = []
+        file = (request.FILES)
+        p = text_extractor(file.values())
+        file_handle = open(p, "rb")
+        response = FileResponse(file_handle, content_type='text')
+        response['Content-Length'] = os.path.getsize(p)
+        response['Content-Disposition'] = 'attachment; filename="%s"' %p
+
+        return response
+        # return Response( p ,status=status.HTTP_200_OK)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
